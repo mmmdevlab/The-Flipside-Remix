2fea7e03b0493d72f0255d05519f1c7cd6f5bcc5
mmmdevlab <mmmdevlab@gmail.com>
2026-02-25 10:50:50 +0800

    Add development files from Building folder

---
+++ Building/script.js
@@ -0,0 +1,20 @@
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+//? score
+//? moves
+//? level
+//? time
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*---------------------------- Functions (Game) ----------------------------*/
+
+//? at the screen1 - press 'play' button->takes them to the game play screen
+
+//? at the screen1 - press hover/click "how to play" button-> overlay instructions - hidden to visible
+
+//? at the screen 1 - press form to type user name + press play button
+
+/*----------------------------- Event Listeners -----------------------------*/
