85bdae83ed4d2928388f89de64968372402fe3c8
mmmdevlab <mmmdevlab@gmail.com>
2026-02-25 17:46:04 +0800

    chore: align CSS with new semantic HTML structure and template logic

--- Building/script.js
+++ Building/script.js
@@ -1,20 +1,50 @@
+//? Arrays for levels, cards, personas
+//? Level Configs: Cards per level, time limits
+//?  - level 1 - 4x3
+//? - cards 12 (Song name vs Artist name)
+//?  - time 60
+//?  - level 2 - 6x3
+//? - cards 18 (Song name vs Artist name)
+//? shuttle feature activation
+//?  - time 90
+//?  - level 3 - 7x4
+//? - cards 28 (Song name vs Artist name)
+//? shuttle feature activation
+//? bonus points
+//?  - time 120
+//? Persona Object:
+//?  - persona 1
+//?  - persona 2
+//?  - persona 3
+//?  - persona 4
+
+//? Track the "Now":
+//? Current Level (1, 2, or 3)
+//? Current Score & Move Count
+//? Time Left (in seconds)
+//? Player Name (Default: "Guest")
+//? Active Cards: First card clicked, Second card clicked (to compare them)
+//? Lock Board: Boolean to prevent clicking a 3rd card during a match check
+//? Game Started: Boolean to check if the timer should begin
+//? Grab the Grid Container
+//? create board:
+//? Grab the Dashboard items (Timer, Score, Moves displays)
+//? Grab all the Screens (Start, Game, Overlays)
+//? start game :
+//? card click:
+//? check match:
+//? timer:
+//? Grab the Template for Personas
+//? cal score to match persona:
+//? Button Triggers :
+//? Board Triggers:
+
 /*-------------------------------- Constants --------------------------------*/
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-//? score
-//? moves
-//? level
-//? time
-
 /*------------------------ Cached Element References ------------------------*/
 
 /*---------------------------- Functions (Game) ----------------------------*/
-
-//? at the screen1 - press 'play' button->takes them to the game play screen
-
-//? at the screen1 - press hover/click "how to play" button-> overlay instructions - hidden to visible
-
-//? at the screen 1 - press form to type user name + press play button
 
 /*----------------------------- Event Listeners -----------------------------*/
