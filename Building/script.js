ff9ccb12f1631489636fbae1c23f92dc0aa3d3f6
mmmdevlab <mmmdevlab@gmail.com>
2026-02-26 17:16:06 +0800

    feat: setup data modules, game state, and dialog overlays

--- Building/script.js
+++ Building/script.js
@@ -1,50 +1,54 @@
-//? Arrays for levels, cards, personas
-//? Level Configs: Cards per level, time limits
-//?  - level 1 - 4x3
-//? - cards 12 (Song name vs Artist name)
-//?  - time 60
-//?  - level 2 - 6x3
-//? - cards 18 (Song name vs Artist name)
-//? shuttle feature activation
-//?  - time 90
-//?  - level 3 - 7x4
-//? - cards 28 (Song name vs Artist name)
-//? shuttle feature activation
-//? bonus points
-//?  - time 120
-//? Persona Object:
-//?  - persona 1
-//?  - persona 2
-//?  - persona 3
-//?  - persona 4
+import * as GameData from "./data.js";
 
-//? Track the "Now":
-//? Current Level (1, 2, or 3)
-//? Current Score & Move Count
-//? Time Left (in seconds)
-//? Player Name (Default: "Guest")
-//? Active Cards: First card clicked, Second card clicked (to compare them)
-//? Lock Board: Boolean to prevent clicking a 3rd card during a match check
-//? Game Started: Boolean to check if the timer should begin
-//? Grab the Grid Container
-//? create board:
-//? Grab the Dashboard items (Timer, Score, Moves displays)
-//? Grab all the Screens (Start, Game, Overlays)
-//? start game :
-//? card click:
-//? check match:
-//? timer:
-//? Grab the Template for Personas
-//? cal score to match persona:
-//? Button Triggers :
-//? Board Triggers:
+console.log(GameData.levels);
+console.log(GameData.personas);
+console.log(GameData.buttonMenu);
 
 /*-------------------------------- Constants --------------------------------*/
 
+//deck array for my cards
+
 /*---------------------------- Variables (state) ----------------------------*/
+
+let playerName = "Guest";
+let currentLevel = 0;
+let score = 0;
+let moves = 0;
+let timer = 0;
+let timeLeft = 0;
+
+//? Active Cards: First card clicked, Second card clicked (to compare them)
+//? Lock Board: Boolean to prevent clicking a 3rd card during a match check
+//? Game Started: Boolean to check if the timer should begin
 
 /*------------------------ Cached Element References ------------------------*/
 
+const screenStart = document.getElementById("screen-start");
+// console.log("start Screen", screenStart);
+const screenGame = document.getElementById("screen-game");
+// console.log("Game Screen", screenGame);
+
+const inputName = document.getElementById("playerName");
+// console.log("input", inputName);
+const btnPlay = document.getElementById("btn-play");
+// console.log("play button", btnPlay);
+const btnHow = document.getElementById("btn-how");
+// console.log("how to play button", btnHow);
+
+//? Grab the Grid Container
+//? Grab the Dashboard items (Timer, Score, Moves displays)
+//? Grab all the Screens (Start, Game, Overlays)
+
 /*---------------------------- Functions (Game) ----------------------------*/
 
+//? create board:
+//? start game :
+//? card click:
+//? check match:
+//? add audio object [cardClick, correctMatch, winLevelSound, lossLevelSound,]
+//? help function called playSound to trigger when interacted -
+
 /*----------------------------- Event Listeners -----------------------------*/
+
+//? Button Triggers :
+//? Board Triggers:
